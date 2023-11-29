bb7f529116ecbe6882e29d177ed02e956ff724c7
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2023-11-29 16:43:38 +0200

    feat: trigger test run on testkube cloud

--- src/main/java/io/testkube/plugins/TestkubeTestRunnerBuilder.java
+++ src/main/java/io/testkube/plugins/TestkubeTestRunnerBuilder.java
@@ -9,6 +9,8 @@ import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -18,25 +20,44 @@ import java.io.IOException;
 public class TestkubeTestRunnerBuilder extends Builder {
 
     private final String apiUrl;
+    private final String orgId;
+    private final String envId;
+    private final String tkNamespace;
     private final String apiToken;
     private final String testName;
 
     @DataBoundConstructor
-    public TestkubeTestRunnerBuilder(String apiUrl, String apiToken, String testName) {
+    public TestkubeTestRunnerBuilder(String apiUrl, String orgId, String envId, String tkNamespace, String apiToken, String testName) {
         this.apiUrl = apiUrl;
+        this.orgId = orgId;
+        this.envId = envId;
+        this.tkNamespace = tkNamespace;
         this.apiToken = apiToken;
         this.testName = testName;
     }
 
     @Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        listener.getLogger().println("Making REST API Call to: " + apiUrl);
+        listener.getLogger().println("Start Testkube test");
+
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet request = new HttpGet(apiUrl);
+            String fullUrl = apiUrl + "/organizations/" + orgId + "/environments/" + envId + "/agent/tests/" + testName
+                    + "/executions";
+
+            listener.getLogger().println("Testkube Test URL: " + fullUrl);
+
+            HttpPost request = new HttpPost(fullUrl);
+            request.setHeader("Authorization", "Bearer " + apiToken);
+            request.setHeader("Content-Type", "application/json");
+
+            String jsonBody = "{\"namespace\": \"" + tkNamespace + "\"}";
+            StringEntity entity = new StringEntity(jsonBody);
+            request.setEntity(entity);
+
             try (CloseableHttpResponse response = httpClient.execute(request)) {
-                listener.getLogger().println("Response: " + response.getStatusLine());
-                // Handle the response as needed
+                listener.getLogger().println("Status: " + response.getStatusLine());
             }
+
         } catch (IOException e) {
             listener.getLogger().println("Error during REST API Call: " + e.getMessage());
             return false;
